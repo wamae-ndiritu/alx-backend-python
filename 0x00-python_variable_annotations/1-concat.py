2ccc190f68d358a7ed3a023a841efa19a402828a
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-14 23:40:33 +0000

    Take strings and return a string concatenated

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module to define the function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
+    return str1 + str2
