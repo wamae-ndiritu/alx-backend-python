650b9e5f9fe87eac126f0a9bc247e5cf32e3cb15
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-14 23:47:38 +0000

    Takes a float argument and return a string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defines to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the given float.
+    """
+    return str(n)
