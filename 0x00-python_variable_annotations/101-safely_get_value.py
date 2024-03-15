6f0e6e027b3fab5d0e163448a6b3777a1717b2bd
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-15 00:58:08 +0000

    Type checking

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,8 @@ from typing import Mapping, Any, TypeVar, Union
 T = TypeVar('~T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[T, None]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[T, None]:
     """
     Safely gets a value from a dictionary.
     Returns the value associated with
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Type checking
+"""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
