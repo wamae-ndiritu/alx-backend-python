3a7627d463577c1bc950b31260fb618e992348c7
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-15 00:05:37 +0000

    Return a tuple in a function

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Define function to_kv
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tupple where the first element is the string k
+    and the second element is the square of v.
+    """
+    return (k, float(v * v))
