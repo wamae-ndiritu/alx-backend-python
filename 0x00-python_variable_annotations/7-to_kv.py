3e51306387a5c00800903504ed58ac1091aeff00
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-15 00:15:22 +0000

    Add whitespace for pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Define function to_kv
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tupple where the first element is the string k
     and the second element is the square of v.
