45f5d06c5db761fb4c2d84810603e430da08a953
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-18 23:01:33 +0000

    Improve the function by using type annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Sleep for a random delay between 0
     and the specified max_delay
